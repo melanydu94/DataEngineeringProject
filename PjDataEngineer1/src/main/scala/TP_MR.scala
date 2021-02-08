5d40a52a0b66ecc8e9478ca931a3b73e09569d73
Alexandra Goncalves <alexandra@iMac-de-Antonio.local>
2021-02-08 11:52:15 +0100

    Initialisation du pj

---
+++ PjDataEngineer1/src/main/scala/TP_MR.scala
@@ -0,0 +1,8 @@
+import scala.collection.MapView
+import scala.io.BufferedSource
+
+object TP_MR {
+  /**
+   * TO DO
+   */
+}
---
+++ PjDataEngineer1/src/test/scala/TP_MR.scala
@@ -0,0 +1,6 @@
+import TP_MR._
+
+class TP_MapReduce_Test extends org.scalatest.FunSuite {
+
+
+}
